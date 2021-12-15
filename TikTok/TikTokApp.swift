c2f0423ab06b907e4e0d8b79a89901fabc3b1dca
Mohit Yadav <iammohit810@gmail.com>
2021-12-15 22:20:53 +0530

    Initial Commit

---
+++ TikTok/TikTokApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokApp.swift
+//  TikTok
+//
+//  Created by Mohit Yadav on 15/12/21.
+//
+
+import SwiftUI
+
+@main
+struct TikTokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
